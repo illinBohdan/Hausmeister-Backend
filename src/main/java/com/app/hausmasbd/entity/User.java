63c6214c98b664335989084349cbaba34ca62f98
Bohdan <blazarchuk@gmail.com>
2026-05-01 18:20:10 +0200

    Add validations to entity User

--- src/main/java/com/app/hausmasbd/entity/User.java
+++ src/main/java/com/app/hausmasbd/entity/User.java
@@ -1,6 +1,10 @@
 package com.app.hausmasbd.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,15 +23,20 @@ public class User {
     private Long id;
 
     @Column(nullable = false)
+    @NotBlank(message = "Der Name darf nicht leer sein.")
+    @Size (min = 2, max = 50)
     private String name;
 
-    @Column(nullable = false)
+    @Column(nullable = true)
+    @Email(message = "Falsches E-Mail-Format")
     private String email;
 
     @Column(nullable = false)
+    @NotBlank(message = "Telefon erforderlich")
+    @Pattern(regexp = "^(\\+?\\d{1,3}[- ]?)?\\d{10}$", message = "Falsches Telefonformat")
     private String phone;
 
     @Column(columnDefinition = "TEXT")
+    @NotBlank(message = "Fügen Sie mindestens ein paar Worte darüber hinzu, was getan werden muss.")
     private String description;
-
 }
